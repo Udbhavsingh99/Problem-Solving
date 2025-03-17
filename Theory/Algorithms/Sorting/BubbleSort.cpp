b93b6c748a9561aed9f512a4c526c8ba915c0183
Udbhavsingh99 <46090896+Udbhavsingh99@users.noreply.github.com>
2025-03-17 18:32:51 +0530

    Update BubbleSort.cpp

--- Theory/Algorithms/Sorting/BubbleSort.cpp
+++ Theory/Algorithms/Sorting/BubbleSort.cpp
@@ -1,1 +1,37 @@
+/*
 
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int size = 0;
+    cout<<"Enter the size of the input array : ";
+    cin>>size;
+
+    int arr[size];
+
+    cout<<"Enter the values in the array : ";
+    for(int i = 0; i<size; i++)
+    {
+        cin>>arr[i];
+    }
+
+    for(int i = 0; i<size; i++)
+    {
+        if(arr[i]>arr[i+1])
+        {
+            int temp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] =temp;
+        }
+    }
+
+    for (int n : arr) 
+    {
+        cout<<n;
+    }
+}
