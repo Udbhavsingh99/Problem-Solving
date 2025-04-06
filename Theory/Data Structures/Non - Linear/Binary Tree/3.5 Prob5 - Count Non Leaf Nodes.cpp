b21697a02effe431bfc4310f701474d42f070442
Udbhav Singh <udbhavravipratapsingh@gmail.com>
2025-04-06 16:38:02 +0530

    Prob5 & Prob6 content

--- Theory/Data Structures/Non - Linear/Binary Tree/3.5 Prob5 - Count Non Leaf Nodes.cpp
+++ Theory/Data Structures/Non - Linear/Binary Tree/3.5 Prob5 - Count Non Leaf Nodes.cpp
@@ -1,5 +1,31 @@
 /*
+Now we want to get the total number of non leaf nodes in our tree.
 
+So, we traverse the tree & for each node we check whether the node has a left & right child node or not. If the tree has atleast one child node ie. 
+the node has either the left child node or the right child node , then we increment the value of count. 
+If the node has neither a left child node nor a right child node , then it implies that this node is a leaf node & hence we don't increment the count 
+variable. 
+
+For each node which is not a leaf node we add 1 to the recursive call to get all the non leaf nodes in the left subtree of the node & the recursive call to get
+all the non leaf nodes in the right subtree of the node. 
+
+If we encounter a leaf node then we return 0 for that recursive subtree call. 
+
+int NonLeaf(Node* root)
+{
+    if(root == NULL)
+    {
+        return 0;
+    }
+
+    if(!root->left && !root->right)
+    {
+        return 0;
+    }
+
+    return (1+NonLeaf(root->left)+NonLeaf(root->right));
+}
+    
 */
 
 #include<iostream>
---
+++ Theory/Data Structures/Non - Linear/Binary Tree/3.6 Prob6 - Height of Binary Tree.cpp
@@ -0,0 +1,5 @@
+/*
+Height of Binary Tree is defined as the distance between the root node & the farthest node from this root node.
+
+
+*/
