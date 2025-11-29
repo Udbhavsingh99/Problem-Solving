1dc890e99eecab9e5d95402c6bbed43f2819c3c8
UdbhavSingh99 <udbhavravipratapsingh@gmail.com>
2025-11-29 18:12:24 +0530

    Added description of other recursion methods

--- Theory/Algorithms/Recursion/Aditya Verma/HypothesisInductionBaseCondition.cpp
+++ Theory/Algorithms/Recursion/Aditya Verma/HypothesisInductionBaseCondition.cpp
@@ -1,4 +1,28 @@
 /*
 New method - Base Condition -> Hypothesis -> Induction
 
+Basically this is how recursion works. 
+Our focus should be on decision making & not on making the input smaller. Input automatically becomes smaller.
+
+Sometimes the decision making process becomes a bit difficult, hence thinking in order to make the input smaller helps to make the task/process easier.
+
+1.Methods to think for a recursive problem : 
+One method is creation of recursive tree. But recursive tree is not the only method , there are other methods too , which are a lot more efficient as well.
+All these methods are a variation on a simple method. Sometimes its easier to use a particular method to think for a problem.
+
+2. Why making input smaller is popular/important
+3. Print 1 to n using recursion
+
+1.Methods to think for a recursive problem : 
+There are 4 approaches to approach a recursive problem - 
+1>The first method is Recursive Tree Method. We use input/output method to create a recursive tree. We make/use the recursive tree when we are able to understand the 
+  decisions that we want to make at each step.
+2>In some situations we cannot understand the decisions that we want to make at each step. We are only able to understand that we need to decrease the size of the input
+  ie . make the input smaller , then in this scenario we can make use of the Base Condition -> Hypothesis -> Induction method.
+3>Choice diagram method 
+4>
+
+3:30
+
+
 */
