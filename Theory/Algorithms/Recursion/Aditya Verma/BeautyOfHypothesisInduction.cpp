e586facba117358420589eda45e7336276ef7f22
UdbhavSingh99 <udbhavravipratapsingh@gmail.com>
2025-12-10 09:00:30 +0530

    Added new Hypothesis

--- Theory/Algorithms/Recursion/Aditya Verma/BeautyOfHypothesisInduction.cpp
+++ Theory/Algorithms/Recursion/Aditya Verma/BeautyOfHypothesisInduction.cpp
@@ -1,4 +1,11 @@
 /*
+When we define a function in the hypothesis stage ie. what the function is supposed to do, then the signature of the function is defined/decided. 
+Once the signature of the function is defined/decided , then the function will work as defined in the hypothesis stage.
 
+We try to print N to 1. 
+
+We first tried to print 1 to N , now we will try to print N to 1.
+To print values 1 to N , during designing the hypothesis  , we called the print() method & passed it value N & in return it will print 1 to N.
+If we run the same print() method with value N-1 ie. print(N-1) , then based on the definition of the method print() , it will print 1 to N-1.
 
 */
