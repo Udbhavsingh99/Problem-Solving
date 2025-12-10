06ad39cbf0858b1df9ecd04aaf40b695175f17cb
UdbhavSingh99 <udbhavravipratapsingh@gmail.com>
2025-12-11 01:33:03 +0530

    Added details for N to 1 method

--- Theory/Algorithms/Recursion/Aditya Verma/BeautyOfHypothesisInduction.cpp
+++ Theory/Algorithms/Recursion/Aditya Verma/BeautyOfHypothesisInduction.cpp
@@ -8,4 +8,12 @@ We first tried to print 1 to N , now we will try to print N to 1.
 To print values 1 to N , during designing the hypothesis  , we called the print() method & passed it value N & in return it will print 1 to N.
 If we run the same print() method with value N-1 ie. print(N-1) , then based on the definition of the method print() , it will print 1 to N-1.
 
+Now we want to print values from N to 1. Then in that case during the hypothesis stage we design the method/function hypothesis ie. the signature of the method. 
+This is the first step ie. hypothesis stage.
+
+If we call the print() method with value N ie. print(N) , then based on the definition of the method print() , it will print N to 1.
+Now if we call the same print() method with value N-1 ie. print(N-1) , then based on the definition of the method print() , it will print N-1 to 1.
+
+2:35
+
 */
