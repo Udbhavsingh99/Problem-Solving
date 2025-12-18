82282c89dc020692fa7c5eb879ef65e373294a89
UdbhavSingh99 <udbhavravipratapsingh@gmail.com>
2025-12-18 08:54:25 +0530

    Sort Array using Recursion

---
+++ Theory/Algorithms/Recursion/Aditya Verma/SortArrayRecursion.cpp
@@ -0,0 +1,19 @@
+/*
+In input we are provided with an array of integers and we have to sort this array using recursion.
+Lets say we have an array arr[] = {2 3 7 6 4 5 9}
+We need to give the sorted array in output ie. arr[] = {2 3 4 5 6 7 9}
+
+We identify a recursive problem if we are provided with choices & we can take decisions on these provided choices.
+
+In the array arr[] = {2 3 7 6 4 5 9} , we can see its a recursive problem , hence we can try 2 methods to solve this problem.
+1. We check if we are able to take decisions on this array to sort it. If we are unable to take decisions on the array then we use
+method 2.
+2. We try to make the input smaller
+
+So we want to make the array sorted ie. arr[] = {2 3 4 5 6 7 9}
+
+Now for this array , the decision making process is not clear/intuitive , hence we use method 2 ie. making the input smaller.
+
+
+4:35
+*/
