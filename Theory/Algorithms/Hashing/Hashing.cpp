6d0af42e72ad54f3d86337d7b6cfdf17963d2a87
Udbhavsingh99 <udbhavravipratapsingh@gmail.com>
2026-01-08 12:11:51 +0530

    Hashing details

--- Theory/Algorithms/Hashing/Hashing.cpp
+++ Theory/Algorithms/Hashing/Hashing.cpp
@@ -1,3 +1,51 @@
 /*
+Initial Problem -
+How many times a particular value occurs in the array.
+
+First Apprroach -
+We compare the input value with value present at each index in the array.
+For this we create a counter variable & initialize to 0 , we then run a for loop iterating from 0 to the
+size of the array & in each iteration we compare the input variable to be searched with the value present at current
+index of the array. If the values are same ie. input variable to be searched = value present at current index of the array,
+we increment the counter variable by 1. Else if the values are different we move to the next iteration leaving the counter
+variable unchanged.
+
+Lets say the input number to be searched is 1 & the input array is ,
+
+|1|2|1|3|2|
+
+first approach is we iterate the entire array using for loop & then we compare with value present at each index in our array.
+So the initial value of i is 0 ie. i = 0, counter variable is 0 & input value to be searched is 1,
+For i = 0, arr[0] = 1 , as 1 = 1 , hence counter++ ie. counter = 1 , i++
+For i = 1, arr[1] = 2 , as 2 != 1, hence counter remains same ie. counter = 1 , i++
+For i = 2, arr[2] = 1 , as 1 = 1 , hence counter++ ie. counter = 2 , i++
+For i = 3, arr[3] = 3 , as 3 != 1, hence counter remains same ie. counter = 2 , i++
+For i = 4, arr[4] = 2 , as 2 != 1, hence counter remains same ie. counter = 2 , i++
+i>size of array , exit counter = 2
+
+Brute force approach :
+
+int func(number, arr)
+{
+	cnt = 0;
+	n = arr.size()
+	for(i = 0; i<n; i++)
+	{
+		if(arr[i] == number)
+		{
+			cnt = cnt + 1;
+		}
+	}
+	return cnt;
+}
+
+We iterate through the entire array & the counter variable will be updated in each iteration after comparison between the number
+passed as argument to the function & the value/number at the current index in the array. If the values are same then the counter
+value is increased/incremented else the counter value remains the same. At the end of the for loop ie. after the entire array
+traversal is completed, the counter value is updated & we return this updated value of counter variable.
+
+We pass the value whose occurence frequency we want to check as the first argument to the function/method.
+It does a linear iteration
+
 
 */
