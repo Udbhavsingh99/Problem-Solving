573d15d8c7c1f73b25faa09f021f833880571008
UdbhavSingh99 <udbhavravipratapsingh@gmail.com>
2026-03-02 01:23:49 +0530

    Added problem solution & details

--- Practice/Leetcode/Easy/Array/two-sum.cpp
+++ Practice/Leetcode/Easy/Array/two-sum.cpp
@@ -1,1 +1,105 @@
 
+//Solution for https://leetcode.com/problems/two-sum/
+
+//Brute Force Solution (Personally submitted)
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> res; 
+        for(int i = 0 ; i<nums.size(); i++)
+        {
+            for(int j = i+1 ; j<nums.size(); j++)
+            {
+                if(i != j)
+                {
+                    if(nums[i]+nums[j] == target)
+                    {   
+                        res.push_back(i);
+                        res.push_back(j);    
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                    
+                }
+                else
+                {
+                    continue;;
+                }
+            }
+        }
+        return res;
+    }
+    
+};
+
+/*
+Optimized approach 1 - 
+Two-pass Hash Table
+
+Intuition
+To improve our runtime complexity, we need a more efficient way to check if the complement exists in the array. If the complement exists, we need to get its index. 
+What is the best way to maintain a mapping of each element in the array to its index? A hash table.
+We can reduce the lookup time from O(n) to O(1) by trading space for speed. A hash table is well suited for this purpose because it supports fast lookup in near constant time. 
+I say "near" because if a collision occurred, a lookup could degenerate to O(n) time. However, lookup in a hash table should be amortized O(1) time as long as the hash function was chosen carefully.
+
+Algorithm
+
+A simple implementation uses two iterations. 
+In the first iteration, we add each element's value as a key and its index as a value to the hash table. 
+Then, in the second iteration, we check if each element's complement (target−nums[i]) exists in the hash table. 
+If it does exist, we return current element's index and its complement's index. 
+Beware that the complement must not be nums[i] itself!
+
+*/
+
+class SolutionOpt1 {
+public:
+    vector<int> twoSum(vector<int> &nums, int target) {
+        unordered_map<int, int> hash;
+        for (int i = 0; i < nums.size(); i++) {
+            hash[nums[i]] = i;
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (hash.find(complement) != hash.end() && hash[complement] != i) {
+                return {i, hash[complement]};
+            }
+        }
+        // If no valid pair is found, return an empty vector
+        return {};
+    }
+};
+
+/*
+Optimized approach 2 -
+
+One-pass Hash Table
+
+It turns out we can do it in one-pass. While we are iterating and inserting elements into the hash table, we also look back to check if current element's complement already exists in the hash table. 
+If it exists, we have found a solution and return the indices immediately.
+*/
+
+class SolutionOpt2 {
+public:
+    vector<int> twoSum(vector<int> &nums, int target) {
+        unordered_map<int, int> hash;
+        for (int i = 0; i < nums.size(); ++i) {
+            int complement = target - nums[i];
+            if (hash.find(complement) != hash.end()) {
+                return {hash[complement], i};
+            }
+            hash[nums[i]] = i;
+        }
+        // Return an empty vector if no solution is found
+        return {};
+    }
+};
--- Theory/Algorithms/Recursion/Aditya Verma/DelMidStackElement.cpp
+++ Theory/Algorithms/Recursion/Aditya Verma/DelMidStackElement.cpp
@@ -1,3 +1,46 @@
 /*
+We are given an input stack with values 5, 4, 3, 2, 1 (top to bottom). We need to pop the values from the stack & print the values , but we need to 
+remove the middle element from the stack. The middle element in this case is 3 , so the output should be 5, 4, 2, 1 (top to bottom).
+
+If the number of elements/values in the stack is odd then the middle element is  (n+1)/2 th element from the top of the stack.
+If the number of elements/values in the stack is even then the middle element is (n/2) th element from the top of the stack. 
+
+Because , lets say we have odd number of elements in the stack , lets say 5 & the elements are -
+
+1 ,2, 3, 4, 5 
+
+Then the middle element is 3 , which is (5+1)/2 = 6/2 = 3rd element from the top of the stack.
+
+If we have even number of elements in the stack , lets say 6 & the elements are - 
+
+1 ,2, 3, 4, 5, 6
+
+Then the middle element is 3 , which is (6/2) = 3rd element from the top of the stack.
+
+But we can instead use a generic formula to find the middle element in the stack, irrespective of whether the number of elements in the
+stack is odd or even. The formula is - 
+
+n/2 + 1
+
+So if the number of elements in the stack is odd , lets say 5 then the middle element is 5/2 + 1 = 2 + 1 = 3rd element from the top of the stack.
+In our example that would be the element 3.
+If the number of elements in the stack is even , lets say 6 then the middle element is 6/2 + 1 = 3 + 1 = 4th element from the top of the stack.
+In our example that would be the element 4.
+
+So we are given an input stack & from this stack we need to delete the middle element. The middle element is given by the formula
+
+k = n/2 + 1
+
+Where n is the number of elements in the stack & k is the position of the middle element from the top of the stack.
+
+In the given example the input stack has 5 elements , so n = 5 & k = 5/2 + 1 = 2 + 1 = 3. So the middle element is the 3rd element from the top of the stack which is the element 3. 
+We need to delete this element from the stack & print the remaining elements in the stack ie. 5, 4, 2, 1 (top to bottom).
+
+When we approach a recursion problem , first we try to think about decision making ie. we try to take decisions. In case we are unable to determine the 
+decision we want to take , then we try to make the size of the input smaller & then we try to solve the problem for the smaller input.
+
+
+3:45
+
 
 */
