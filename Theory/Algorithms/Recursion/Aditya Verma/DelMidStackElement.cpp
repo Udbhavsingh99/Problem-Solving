73cedf2623356c7134a8bf56a5859c2849610854
UdbhavSingh99 <udbhavravipratapsingh@gmail.com>
2026-03-18 09:35:58 +0530

    Added stack deletion recursion

--- Theory/Algorithms/Recursion/Aditya Verma/DelMidStackElement.cpp
+++ Theory/Algorithms/Recursion/Aditya Verma/DelMidStackElement.cpp
@@ -69,9 +69,56 @@ Hence we will keep on passing k-1 as the second argument to the solve() method i
 This recursive call to the solve() method on the smaller input stack will return us a stack with the middle element removed from the smaller input stack. Hence the recursive call to the solve() method with smaller input stack ie. 4, 3, 2, 1 (top to bottom) will return us a stack with the middle element removed from this smaller input stack. 
 The middle element in this smaller input stack is the second element from the top of the stack which is the element 3. Hence the recursive call to the solve() method on this smaller input stack will return us a stack with the element 3 removed from it ie. 4, 2, 1 (top to bottom).
 
+Once we get the stack with the middle element ie. 3 removed from the smaller input stack ie. 4, 2, 1 (top to bottom), now we will add/push the top most element that we had stored in a variable before making the input stack smaller back to this stack. In our case we will push the top most element 5 that we removed earlier , to make the input stack smaller, back to the stack with the middle
+element removed from it ie. 4, 2, 1 (top to bottom). Hence we will push the element 5 back to this stack to get the final output stack with the middle element removed from the original input stack ie. 5, 4, 2, 1 (top to bottom).
+
+Hypothesis - 
+We are provided a function solve() which takes a stack s & the position of the middle element k as the arguments ie. solve(s, k). We will call this same solve() method for a smaller input.
+Now this solve() method will return us a stack with the middle element removed from the smaller input stack. 
+We will store the top most element from the original input stack in a variable & then we will pop the top most element from the original input stack to make the input smaller. Then we will call the solve() method recursively on the smaller input stack ie. solve(s, k-1) to get a stack with the middle element removed from the smaller input stack. 
+Once we get the stack with the middle element removed from the smaller input stack, we will push the top most element that we had stored in a variable (to make the input stack smaller) back to this stack to get the final output stack with the middle element removed from the original input stack.
 
 
-7:24
+Base Condition - 
+In the hypothesis we can observe that with each consecutive recursive call to the solve() method on the smaller input stack, the value of k ie. the position of the middle element from the top of the stack keeps on decreasing by 1.
+Then the smallest possible value of k is 1, which means that the middle element from the top of the stack is the top most element of the stack. Hence when k becomes 1 , we will pop the top most element from the stack to remove the middle element from the stack & then we will return this stack with the middle element removed from it.
+
+Now for a given input stack with 5 elements ie. 5, 4, 3, 2, 1 (top to bottom) , the value of k tells us/ determines the position of the middle element that we want to delete from the stack , relative to the top most element in the stack ie. size of the stack. In our case the position of the middle element ie. k is 3, which means that the middle element is the 3rd element from the top of 
+the stack which is the element 3. Hence we will pass the value of k as 3 to the solve() method as the second argument when we call the solve() method for the first time with the original input stack.
+
+Now we will store the top most element from the original input stack ie. 5 in a variable & then we will pop the top most element from the original input stack to make the input smaller. Now we have a smaller input stack with values 4, 3, 2, 1 (top to bottom) & the position of the middle element from the top of the stack is now k-1 = 3-1 = 2.
+Hence we will call the solve() method recursively on this smaller input stack with the value of k as 2 ie. solve(s, 2) to get a stack with the middle element removed from this smaller input stack.
+
+We again make a recursive call to the solve() method on a smaller input stack with the value of k as 1 ie. solve(s, 1). We make the input stack smaller by again storing the top most element from the smaller input stack ie. 4 in a variable & then popping the top most element from the smaller input stack to make the input smaller.
+Now we have a smaller input stack with values 3, 2, 1 (top to bottom) & the position of the middle element from the top of the stack is now k-1 = 2-1 = 1. 
+
+Now the value of k is 1 which means that the middle element from the top of the stack is the top most element of the smaller stack & hence we want to delete it. We pop the top most element from the smaller input stack to remove the middle element from this smaller input stack & then we return this stack with the middle element removed from it ie. 2, 1 (top to bottom).
+Hence the smallest possible value of k is 1 which is the base condition for our recursive solution.
+
+So if we are provided input stack 5, 4, 3, 2, 1 (top to bottom) & the value of k as 3 , then we first store the top most element from the original input stack ie. 5 in a variable & then we pop the top most element from the original input stack to make the input smaller. 
+Now we have a smaller input stack with values 4, 3, 2, 1 (top to bottom) & the position of the middle element from the top of the stack is now k-1 = 3-1 = 2. Hence we will call the solve() method recursively on this smaller input stack with the value of k as 2 ie. solve(s, 2) to get a stack with the middle element removed from this smaller input stack.
+Now we store the top most element from this smaller input stack ie. 4 in a variable & then we pop the top most element from this smaller input stack to make the input smaller. Now we have a smaller input stack with values 3, 2, 1 (top to bottom) & the position of the middle element from the top of the stack is now k-1 = 2-1 = 1.
+Hence we will call the solve() method recursively on this smaller input stack with the value of k as 1 ie. solve(s, 1) to get a stack with the middle element removed from this smaller input stack. 
+Now as value of k is 1 which means that middle element from the top of the stack is the top most element of the smaller stack & hence we want to delete it ie. pop it. Then we insert the values that we removed from the input stack to make the input stack smaller ie. 4 & 5 back to the stack with the middle element removed from it ie. 2, 1 (top to bottom) to get the final
+output stack with the middle element removed from the original input stack ie. 5, 4, 2, 1 (top to bottom).
+
+The base condition is, 
+
+if(k == 1) {
+    s.pop();
+    return s;
+}
+    
+We call the solve() method & provide the input stack ie. 5, 4, 3, 2, 1 (top to bottom) & the value of k ie. the position of the middle element from the top that we want to delete as 3. This method will delete the 3rd element from the top of the stack which is the element 3. 
+This solve() method will return us a stack with the middle element removed from the original input stack ie. 5, 4, 2, 1 (top to bottom).
+
+We call the solve() method for a smaller input. We store the top most element from the original input stack in a variable & then we pop the top most element from the original input stack to make the input smaller. Then we call the solve() method recursively on the smaller 
+input stack ie. solve(s, k-1) as k-1 is now the position of the middle element that we want to delete from the top of the input stack, to get a stack with the middle element removed from the smaller input stack.
+
+Induction step - 
+In the induction step we will push the top most element that we had stored in a variable (to make the input stack smaller) back to the stack with the middle element removed from it to get the final output stack with the middle element removed from the original input stack.
+
+11:50
 
 
 */
