0d4fdd0632d13e57620b9a4e79483c2f4eda9ddc
UdbhavSingh99 <udbhavravipratapsingh@gmail.com>
2026-03-29 21:57:42 +0530

    Added new file

---
+++ Theory/Algorithms/Recursion/Aditya Verma/ReverseStack.cpp
@@ -0,0 +1,16 @@
+/*
+
+Input - 
+In the input we are provided a stack with elements in it. Lets say the elements present in the input stack are 5, 4, 3, 2, 1 (top to bottom)
+
+Output - 
+In the output we need to return the stack in reverse order ie. all the elements in the input stack should be in reverse order in the output stack  ie.
+1, 2, 3, 4, 5 (top to bottom)
+
+Constraint - We cannot use any extra space to reverse the stack ie. we cannot use a separate stack or a separate array to reverse the stack. 
+We can only use the given input stack to reverse it.
+
+We can use recursion here to reverse the stack. 
+
+2:24
+*/
