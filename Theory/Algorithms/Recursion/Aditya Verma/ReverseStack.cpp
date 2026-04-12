099f8e78965d59f65402eb16a98cb01619e89c75
UdbhavSingh99 <udbhavravipratapsingh@gmail.com>
2026-04-13 00:59:32 +0530

    Added recursion & tree details

--- Theory/Algorithms/Recursion/Aditya Verma/ReverseStack.cpp
+++ Theory/Algorithms/Recursion/Aditya Verma/ReverseStack.cpp
@@ -10,7 +10,47 @@ In the output we need to return the stack in reverse order ie. all the elements 
 Constraint - We cannot use any extra space to reverse the stack ie. we cannot use a separate stack or a separate array to reverse the stack. 
 We can only use the given input stack to reverse it.
 
-We can use recursion here to reverse the stack. 
+We can use recursion here to reverse the stack.
 
-2:24
+Recursion will use extra space of its own ie. it will require extra space for the recursive call stack of its own, but the problem will be solved in O(1)
+space complexity as we are not using any extra space to reverse the stack.
+
+1. Hypothesis - 
+We have a method that will reverse the input stack and return the reversed stack.
+Lets say the input stack that we want to reverse is 5, 4, 3, 2, 1 (top to bottom) , then when we call the method to reverse the stack, 
+it will return the reversed stack 1, 2, 3, 4, 5 (top to bottom)
+
+Now in hypothesis we will make sure that the reverse method works for smaller inputs as well.
+In order to make the input stack smaller , we will pop the top element of the stack & then call the reverse method on the remaining smaller stack.
+
+We will store the top most element from the input stack in a variable & then pop the top element from the input stack to make the input stack smaller. 
+Then we will call the reverse method on the smaller input  ie. 4, 3, 2, 1 (top to bottom) to reverse it.
+This reverse method will return the reversed stack 1, 2, 3, 4 (top to bottom) 
+
+2. Induction -
+In the induction step, we will push the top most element that we stored in the variable (which is 5) to the bottom of the reversed stack 1, 2, 3, 4 (top to bottom) 
+to get the final reversed stack 1, 2, 3, 4, 5 (top to bottom)
+
+Now in order to insert the value 5 that we removed from the input stack in order to make the input stack smaller, back into the sorted stack 1, 2, 3, 4 (top to bottom) , 
+we will use another recursive method that will insert the value 5 at the bottom of the reversed stack 1, 2, 3, 4 (top to bottom)
+
+In order to insert the value 5 at the bottom of the reversed stack 1, 2, 3, 4 (top to bottom) , we will need to pop all the elements from the reversed stack 1, 2, 3, 4 (top to bottom)
+to make the reversed stack empty, then we will push the value 5 to the empty reversed stack & then we will push all the popped elements back to the reversed stack 1, 2, 3, 4 (top to bottom) 
+to get the final reversed stack 1, 2, 3, 4, 5 (top to bottom)
+
+So we are provided with an input stack 1, 2, 3, 4 (top to bottom) & the value to be inserted at the bottom of the reversed stack ie. 5 is provided as input to this method.
+Now the stack returned as output from this method will be the reversed stack 1, 2, 3, 4, 5 (top to bottom)
+
+Again in order to write this method we will first need to design a hypothesis for this method as well.
+Lets say we define a method insert() such that it takes the input stack 1, 2, 3, 4 (top to bottom) as the first argument & the value to be inserted at the bottom of the reversed stack ie.
+5 as the second argument, then this method will return the reversed stack 1, 2, 3, 4, 5 (top to bottom) as  ie. it will insert the value 5 at the bottom of the input stack.
+
+Now in the hypothesis we will make sure that the insert() method works for smaller inputs as well.
+In order to make the input stack smaller , we will first store the top element of the stack in a variable, then pop the top element of the stack & then call the insert() method on the remaining smaller stack. 
+
+We call the insert() method on the smaller input stack 2, 3, 4 (top to bottom) to insert the value 5 at the bottom of the smaller input stack 2, 3, 4 (top to bottom)
+
+
+
+5:26
 */
--- Theory/Data Structures/Non - Linear/Binary Tree/4.1 Prob1 - TwoTreesIdentical.cpp
+++ Theory/Data Structures/Non - Linear/Binary Tree/4.1 Prob1 - TwoTreesIdentical.cpp
@@ -102,8 +102,18 @@ returned by the values returned by the left & right subtree ie. left & right chi
 Now we do the AND ie. && operation because , if the value at any particular node is not equal ie. different in tree 1 & tree 2, then in that case instead of returning 1, we return 0 & return ie. we don't need to check the value of any other further
 nodes in the tree. Now if the value at a particular given node is equal , then in that case we check for the equality of the left subtree of that node & the right subtree of that node.
 
+We take the AND operation ie. && between the values returned by the left & right subtree of a particular node & then return the resultant of this AND operation to the parent node of that particular node.
+So if the left & right subtree of a particular node are identical ie. both the left & right subtree of a particular node return the value 1 , then in that scenario L && R becomes 1 && 1 which is equal to 1
+& hence the value 1 is returned to the parent node of that particular node.
+If any one of the left or the right subtree of a particular node is not identical ie. either the left subtree or the right subtree of a particular node returns the value 0 , then in that scenario L && R becomes 1 && 0 or 0 && 1 which is equal to 0.
+We do this for all the nodes in the trees & if we get the final value as 1 then we say that both the trees are identical else if we get the final value as 0 then we say that both the trees are not identical.
 
-7:37
+We also check the values at the present nodes as well. If the values at the present nodes are different in both the trees then we return 0 without checking for the left & right subtree of that particular node. 
+If the values at the present nodes are same in both the trees then we check for the left & right subtree of that particular node.
+
+
+
+9:46
 
 
 */
